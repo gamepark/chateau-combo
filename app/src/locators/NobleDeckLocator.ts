3fa540accb32f8dabe7dc5a04f441787d8091741
Patrick Beaume <p.beaume@gmail.com>
2024-07-29 17:36:07 +0200

    disable sort for deck

--- app/src/locators/NobleDeckLocator.ts
+++ app/src/locators/NobleDeckLocator.ts
@@ -4,6 +4,7 @@ import { cardDescription } from '../material/CardDescription'
 export class NobleDeckLocator extends DeckLocator {
   coordinates = { x: -(cardDescription.width + 0.7), y: -18, z: 0 }
   delta = { x: -0.03, y: -0.03, z: 0.1 }
+  navigationSorts = []
 }
 
 export const nobleDeckLocator = new NobleDeckLocator()
