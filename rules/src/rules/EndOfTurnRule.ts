4c350841c4c5d9274803c9be9ebebb13995222fd
Tho33 <theo.gregorio@laposte.net>
2024-08-22 09:52:54 +0200

    Add move Messenger if it should

--- rules/src/rules/EndOfTurnRule.ts
+++ rules/src/rules/EndOfTurnRule.ts
@@ -3,11 +3,22 @@ import { LocationType } from '../material/LocationType'
 import { MaterialType } from '../material/MaterialType'
 import { Memory } from './Memory'
 import { RuleId } from './RuleId'
+import { BannerType, cardCharacteristics } from '../CardCharacteristics'
+import { isNoble } from '../Card'
 
 export class EndOfTurnRule extends PlayerTurnRule {
   onRuleStart() {
     const moves: MaterialMove[] = []
 
+    // Move Messenger if it should
+    if (this.placedCard?.id !== undefined && cardCharacteristics[this.placedCard.id].canSwapMessengerToken === true) {
+      moves.push(this.messenger.moveItem({
+        type: LocationType.EndOfRiver,
+        id: isNoble(this.placedCard.id) ? BannerType.VillageBanner : BannerType.NobleBanner
+      }))
+    }
+
+    // Refill villageRiver
     const villageRiver = this.villageRiver
     const villageDeck = this.villageDeck
     const villageToDraw = 3 - villageRiver.length
@@ -19,6 +30,7 @@ export class EndOfTurnRule extends PlayerTurnRule {
       )
     }
 
+    // Refill NobleRiver
     const nobleRiver = this.nobleRiver
     const nobleDeck = this.nobleDeck
     const nobleToDraw = 3 - nobleRiver.length
@@ -30,13 +42,17 @@ export class EndOfTurnRule extends PlayerTurnRule {
       )
     }
 
-    this.forget(Memory.PlacedCard)
-
     moves.push(
       this.startPlayerTurn(RuleId.SpendKey, this.nextPlayer)
     )
 
     return moves
+  }
+
+  get messenger() {
+    return this
+      .material(MaterialType.MessengerToken)
+      .location(LocationType.EndOfRiver)
   }
 
   get nobleDeck() {
@@ -65,8 +81,16 @@ export class EndOfTurnRule extends PlayerTurnRule {
       .location(LocationType.VillageRiver)
   }
 
+  get placedCard() {
+    return this
+      .material(MaterialType.Card)
+      .getItem(this.remind(Memory.PlacedCard))
+  }
+
   onRuleEnd() {
+    // Cleaning
     this.forget(Memory.PlacedCard)
+    this.forget(Memory.ImmediateEffectsToPlay)
     return []
   }
 }
