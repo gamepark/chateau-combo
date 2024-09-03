90a82201aa95babaa0807ed2d730d520b48dc4b5
Romain Fromi <romain.fromi@gmail.com>
2024-09-03 17:51:27 +0200

    Prepare to display Key3 & Gold5 images

--- app/src/material/GoldCoinDescription.tsx
+++ app/src/material/GoldCoinDescription.tsx
@@ -1,13 +1,31 @@
 import { LocationType } from '@gamepark/chateau-combo/material/LocationType'
-import { RoundTokenDescription } from '@gamepark/react-game'
+import { ComponentSize, TokenDescription } from '@gamepark/react-game'
+import { MaterialItem } from '@gamepark/rules-api'
 import Gold1 from '../images/tokens/Gold1.png'
+import Gold5 from '../images/tokens/Gold5.png'
 
-export class GoldCoinDescription extends RoundTokenDescription {
-    diameter = 1.9
-    image = Gold1
+export class GoldCoinDescription extends TokenDescription {
+  images = {
+    1: Gold1,
+    5: Gold5
+  }
 
-    stockLocation = { type: LocationType.GoldStock }
-    staticItem = { quantity: 10, location: this.stockLocation }
+  getSize(itemId: number): ComponentSize {
+    return itemId === 5 ? { width: 2.6, height: 2.6 } : { width: 1.9, height: 1.9 }
+  }
+
+  getStockLocation(item: MaterialItem) {
+    return { type: LocationType.GoldStock, id: item.id ?? 1 }
+  }
+
+  staticItems = [
+    { id: 1, quantity: 10, location: { type: LocationType.GoldStock, id: 1 } },
+    { id: 5, quantity: 5, location: { type: LocationType.GoldStock, id: 5 } }
+  ]
+
+  protected getFrontId(itemId: number): number {
+    return itemId ?? 1
+  }
 }
 
 export const goldCoinDescription = new GoldCoinDescription()
--- app/src/material/KeyDescription.tsx
+++ app/src/material/KeyDescription.tsx
@@ -1,15 +1,31 @@
 import { LocationType } from '@gamepark/chateau-combo/material/LocationType'
-import { TokenDescription } from '@gamepark/react-game'
+import { ComponentSize, TokenDescription } from '@gamepark/react-game'
+import { MaterialItem } from '@gamepark/rules-api'
 import Key1 from '../images/tokens/Key1.png'
+import Key3 from '../images/tokens/Key3.png'
 
 export class KeyDescription extends TokenDescription {
-    height = 3
-    width = 1.67
-    image = Key1
-    borderRadius = 1
+  images = {
+    1: Key1,
+    3: Key3
+  }
 
-    stockLocation = { type: LocationType.KeyStock }
-    staticItem = { quantity: 10, location: this.stockLocation }
+  getSize(itemId: number): ComponentSize {
+    return itemId === 3 ? { width: 3.315, height: 3.4 } : { width: 1.67, height: 3 }
+  }
+
+  getStockLocation(item: MaterialItem) {
+    return { type: LocationType.KeyStock, id: item.id ?? 1 }
+  }
+
+  staticItems = [
+    { id: 1, quantity: 10, location: { type: LocationType.KeyStock, id: 1 } },
+    { id: 3, quantity: 5, location: { type: LocationType.KeyStock, id: 3 } }
+  ]
+
+  protected getFrontId(itemId: number): number {
+    return itemId ?? 1
+  }
 }
 
 export const keyDescription = new KeyDescription()
