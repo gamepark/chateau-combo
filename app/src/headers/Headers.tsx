d28be4bb132cc1dcfb37427129b3baa8d193441a
Patrick Beaume <p.beaume@gmail.com>
2024-09-05 22:14:14 +0200

    move messenger header

--- app/src/headers/Headers.tsx
+++ app/src/headers/Headers.tsx
@@ -7,6 +7,7 @@ import { DiscardFromRiverHeader } from './DiscardFromRiverHeader'
 import { EndGameHeader } from './EndGameHeader'
 import { EndOfTurnHeader } from './EndOfTurnHeader'
 import { KeyEffectHeader } from './KeyEffectHeader'
+import { MoveMessengerHeader } from './MoveMessengerHeader'
 import { SpendKeyHeader } from './SpendKeyHeader'
 
 export const Headers: Partial<Record<RuleId, ComponentType>> = {
@@ -15,6 +16,7 @@ export const Headers: Partial<Record<RuleId, ComponentType>> = {
   [RuleId.BuyCard]: BuyCardHeader, // TODO header plus spécifique
   [RuleId.DiscardFromRiver]: DiscardFromRiverHeader,
   [RuleId.ChooseBetween]: ChooseBetweenHeader,
+  [RuleId.MoveMessenger]: MoveMessengerHeader,
   [RuleId.EndOfTurn]: EndOfTurnHeader,
-  [RuleId.EndGame]:EndGameHeader
+  [RuleId.EndGame]: EndGameHeader
 }
---
+++ app/src/headers/MoveMessengerHeader.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+
+import { Trans } from 'react-i18next'
+
+export const MoveMessengerHeader = () => {
+  return (
+    <Trans defaults="move-messenger" />
+  )
+}
