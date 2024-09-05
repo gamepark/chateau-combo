5e832cb3e67d8efe1f28f4098fcb3f1772bb8e54
Romain Fromi <romain.fromi@gmail.com>
2024-09-05 18:21:54 +0200

    Display points instead of keys once game is over

--- app/src/panels/ChateauComboPlayerPanel.tsx
+++ app/src/panels/ChateauComboPlayerPanel.tsx
@@ -1,16 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { ChateauComboRules } from '@gamepark/chateau-combo/ChateauComboRules'
-import { MaterialType } from '@gamepark/chateau-combo/material/MaterialType'
 import { PlayerBoardHelper } from '@gamepark/chateau-combo/rules/helpers/PlayerBoardHelper'
 import { Player } from '@gamepark/react-client'
 import { CounterProps, StyledPlayerPanel, useRules } from '@gamepark/react-game'
 import { FC, HTMLAttributes, useMemo } from 'react'
+import pointImage from '../images/icons/point.png'
 import coinImage from '../images/tokens/Gold1.png'
 import keyImage from '../images/tokens/Key1.png'
 
 type ChateauComboPlayerPanelProps = {
-    player: Player
-  } & HTMLAttributes<HTMLDivElement>
+  player: Player
+} & HTMLAttributes<HTMLDivElement>
 
 export const ChateauComboPlayerPanel: FC<ChateauComboPlayerPanelProps> = (props) => {
   const { player, ...rest } = props
@@ -18,10 +18,10 @@ export const ChateauComboPlayerPanel: FC<ChateauComboPlayerPanelProps> = (props)
   const state = useMemo(() => new PlayerBoardHelper(rules.game, player.id), [rules.game, player.id])
 
   const counters: CounterProps[] = [{
-    image: tokenImage(MaterialType.Key),
-    value: state.keyQuantity
+    image: rules.isOver() ? pointImage : keyImage,
+    value: rules.isOver() ? rules.getScore(player.id) : state.keyQuantity
   }, {
-    image: tokenImage(MaterialType.GoldCoin),
+    image: coinImage,
     value: state.coinsQuantity
   }]
 
@@ -35,5 +35,3 @@ export const ChateauComboPlayerPanel: FC<ChateauComboPlayerPanelProps> = (props)
     />
   )
 }
-
-const tokenImage = (token:MaterialType):string => token === MaterialType.GoldCoin ? coinImage : keyImage
