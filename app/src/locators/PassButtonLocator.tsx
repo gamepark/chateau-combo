3b6920566c7d3753ef20e8eb9bbcfb4a1ee11b98
Romain Fromi <romain.fromi@gmail.com>
2026-04-10 16:40:54 +0200

    Fix Pass button location

--- app/src/locators/PassButtonLocator.tsx
+++ app/src/locators/PassButtonLocator.tsx
@@ -1,17 +1,14 @@
 import { css } from '@emotion/react'
-import { LocationType } from '@gamepark/chateau-combo/material/LocationType'
-import { MaterialType } from '@gamepark/chateau-combo/material/MaterialType'
-import { CustomMoveType } from '@gamepark/chateau-combo/rules/CustomMoveType'
-import { RuleId } from '@gamepark/chateau-combo/rules/RuleId'
 import { faForwardStep } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { LocationType } from '@gamepark/chateau-combo/material/LocationType'
+import { CustomMoveType } from '@gamepark/chateau-combo/rules/CustomMoveType'
+import { RuleId } from '@gamepark/chateau-combo/rules/RuleId'
 import { LocationDescription, Locator, MaterialContext, useLegalMove } from '@gamepark/react-game'
-import { isCustomMoveType, Location } from '@gamepark/rules-api'
+import { isCustomMoveType } from '@gamepark/rules-api'
 import { Trans } from 'react-i18next'
 import { SealMenuButton } from '../theme/SealMenuButton'
-import { cardDescription } from '../material/ChateauComboCardDescription'
 import { getViewPlayer } from './panelCoordinates'
-import { TABLEAU_X } from './TableauLocator'
 
 const PassButton = () => {
   const passMove = useLegalMove(m => isCustomMoveType(CustomMoveType.Pass)(m))
@@ -25,33 +22,7 @@ class PassButtonLocator extends Locator {
 
   locationDescription = new PassButtonDescription()
 
-  getCoordinates(location: Location, context: MaterialContext) {
-    const { rules } = context
-    const player = location.player!
-    const cards = rules.material(MaterialType.Card)
-      .location(LocationType.Tableau)
-      .player(player)
-      .getItems()
-
-    const gap = { x: cardDescription.width + 0.2, y: cardDescription.height + 0.2 }
-
-    if (cards.length === 0) {
-      return { x: TABLEAU_X, y: 6.5 + gap.y + 1.5, z: 5 }
-    }
-
-    const xs = cards.map(c => c.location.x ?? 0)
-    const ys = cards.map(c => c.location.y ?? 0)
-    const minX = Math.min(...xs)
-    const maxX = Math.max(...xs)
-    const maxY = Math.max(...ys)
-    const centerX = (minX + maxX) / 2
-
-    return {
-      x: TABLEAU_X + centerX * gap.x + 1,
-      y: 6.5 + (maxY + 1) * gap.y + 1.5,
-      z: 5
-    }
-  }
+  coordinates = { x: -19, y: 25, z: 1 }
 
   getLocations(context: MaterialContext) {
     const { rules, player } = context
